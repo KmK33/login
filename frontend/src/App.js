9ec2cf5ede70d7d9a8e7ef132a88c4dfc83f10f2
KmK33 <kmk112003@gmail.com>
2023-11-04 23:05:29 +0530

    exams xD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Login1 from './component/Login/Login1';
-import Signup from './component/Login/Signup';
-
+import Login1 from "./component/Login/Login1";
+import Signup from "./component/Login/Signup";
+import Home from "./component/pages/Home";
 
 function App() {
   return (
     <div className="App">
-      
-      
-      
       <Router>
-      
-      
         <Routes>
-
-
-
-          <Route path='/login' element={<Login1 /> } />
-            <Route path='/signup' element={<Signup /> } />
-
-
+          <Route path="/login" element={<Login1 />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
-      
-    </Router>
-
+      </Router>
     </div>
   );
 }
